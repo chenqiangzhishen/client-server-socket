07186d45439c20f3f3e2fdbacaa96ffa315610b0
ChenQiang <chenqiangzhishen@163.com>
2015-05-14 16:37:48 -0400

    c++ socket client & server

---
+++ socket/ServerMain.cpp
@@ -0,0 +1,41 @@
+#include "ServerSocket.h"
+#include "SocketException.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ( void )
+{
+    std::cout << "running....\n";
+
+    try
+    {
+        // Create the socket
+        ServerSocket server ( 30000 );
+
+        while ( true )
+        {
+
+            ServerSocket new_sock;
+            server.accept ( new_sock );
+
+            try
+            {
+                while ( true )
+                {
+                    std::string data;
+                    new_sock >> data;
+                    new_sock << data;
+                }
+            }
+            catch ( SocketException& ) {}
+
+        }
+    }
+    catch ( SocketException& e )
+    {
+        std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";
+    }
+
+    return 0;
+}
