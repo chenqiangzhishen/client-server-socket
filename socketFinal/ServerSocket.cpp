44190f89a949b182dd685d01a0a5b74b8d4e0430
chen qiang <chenqiangzhishen@163.com>
2015-05-26 11:12:10 +0800

    add new branch - test

--- socketFinal/ServerSocket.cpp
+++ socketFinal/ServerSocket.cpp
@@ -9,21 +9,21 @@ using namespace std;
 ServerSocket::ServerSocket ( )
 {
     /*
-        if ( ! Socket::create() )
-        {
-            throw SocketException ( "Could not create server socket." );
-        }
+       if ( ! Socket::create() )
+       {
+       throw SocketException ( "Could not create server socket." );
+       }
 
-        if ( ! Socket::bind ( port ) )
-        {
-            throw SocketException ( "Could not bind to port." );
-        }
+       if ( ! Socket::bind ( port ) )
+       {
+       throw SocketException ( "Could not bind to port." );
+       }
 
-        if ( ! Socket::listen() )
-        {
-            throw SocketException ( "Could not listen to socket." );
-        }
-    	*/
+       if ( ! Socket::listen() )
+       {
+       throw SocketException ( "Could not listen to socket." );
+       }
+       */
 
     m_listen_sock = 0;
     m_epoll_fd = 0;
@@ -75,21 +75,21 @@ bool ServerSocket::init(int port , int sock_count)
         exit(1);
     }
     /*
-    if ( ! Socket::create() )
-    {
-        throw SocketException ( "Could not create server socket." );
-    }
+       if ( ! Socket::create() )
+       {
+       throw SocketException ( "Could not create server socket." );
+       }
 
-    if ( ! Socket::bind ( port ) )
-    {
-        throw SocketException ( "Could not bind to port." );
-    }
+       if ( ! Socket::bind ( port ) )
+       {
+       throw SocketException ( "Could not bind to port." );
+       }
 
-    if ( ! Socket::listen() )
-    {
-        throw SocketException ( "Could not listen to socket." );
-    }
-    */
+       if ( ! Socket::listen() )
+       {
+       throw SocketException ( "Could not listen to socket." );
+       }
+       */
 
     m_epoll_events = new struct epoll_event[sock_count];
     if (m_epoll_events == NULL)
@@ -261,13 +261,80 @@ int ServerSocket::epoll_server_wait(int time_out)
     return epoll_wait(m_epoll_fd, m_epoll_events, m_max_count, time_out);
 }
 
-void ServerSocket::run(int time_out)
+void* ServerSocket::pthread_handle_message(void* para)
 {
     char *recv_buf = new char[65535];
     char *send_buf = new char[65535];
     string recv_str;
-    int ret;
     int key_id;
+
+    pthreadParm *pp = (pthreadParm *)para;
+    bool flag_in = true;
+    bool flag_out = true;
+
+    while(flag_in || flag_out)
+    {
+        if(pp->m_ee->events & EPOLLIN)
+        {
+            int recv_count = pp->m_ss->recv_data(pp->m_ee->data.fd, recv_buf);
+            cout<<recv_buf<<endl;
+
+            Json::Reader reader;
+            Json::Value value;
+            //Json format deserialization from string to Json
+            if(reader.parse(recv_buf, value))
+            {
+                //server key_id ++ ;
+                key_id = value["key_id"].asInt() + 1;
+            }
+
+            sm::SocketMessage sockMessage;
+            // Server initialize protobuf format.
+            sockMessage.set_received_key_id_plus_1(key_id);
+            // serialize the protobuf format to string.
+            if( !sockMessage.SerializeToString(&recv_str))
+            {
+                cerr << "Failed to write protobuf" << endl;
+                return NULL;
+            }
+
+            strcpy(send_buf, recv_str.c_str());
+            memset(recv_buf, 0, sizeof(recv_buf));
+            flag_in = false;
+        }
+        else if(pp->m_ee->events & EPOLLOUT)
+        {
+            int send_count = pp->m_ss->send_data(pp->m_ee->data.fd, send_buf, strlen(send_buf));
+            memset(send_buf, 0, sizeof(send_buf));
+            flag_out = false;
+        }
+    }
+    close(pp->m_ee->data.fd);
+    if(recv_buf)
+    {
+        delete []recv_buf;
+        recv_buf = NULL;
+    }
+    if(send_buf)
+    {   delete []send_buf;
+        send_buf = NULL;
+    }
+}
+void ServerSocket::run(int time_out)
+{
+    //char *recv_buf = new char[65535];
+    //char *send_buf = new char[65535];
+    //string recv_str;
+    int ret;
+    //int key_id;
+
+    pthread_mutex_t thread_mutex;
+    if(0 != pthread_mutex_init(&thread_mutex, NULL))
+    {
+        cerr << "thread mutex initialization failed" << endl;
+        exit(EXIT_FAILURE);
+    }
+
     while(1)
     {
         int ret = epoll_server_wait(time_out);
@@ -284,6 +351,7 @@ void ServerSocket::run(int time_out)
         {
             for(int i = 0; i < ret; i++)
             {
+
                 if(m_epoll_events[i].data.fd == m_listen_sock)
                 {
                     if(m_epoll_events[i].events & EPOLLIN)
@@ -293,39 +361,22 @@ void ServerSocket::run(int time_out)
                 }
                 else
                 {
-                    if(m_epoll_events[i].events & EPOLLIN)
+                    pthread_mutex_lock(&thread_mutex);
+                    pthread_attr_t attr;
+                    pthread_t threadId;
+
+                    pthread_attr_init(&attr);
+                    pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+                    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+                    pthreadParm pp;
+                    pp.m_ss = this;
+                    pp.m_ee = &m_epoll_events[i];
+                    if(pthread_create(&threadId, &attr, pthread_handle_message, (void*)&(pp)))
                     {
-                        int recv_count = recv_data(m_epoll_events[i].data.fd, recv_buf);
-                        cout<<recv_buf<<endl;
-
-                        Json::Reader reader;
-                        Json::Value value;
-                        //Json format deserialization from string to Json
-                        if(reader.parse(recv_buf, value))
-                        {
-                            //server key_id ++ ;
-                            key_id = value["key_id"].asInt() + 1;
-                        }
-
-                        sm::SocketMessage sockMessage;
-                        // Server initialize protobuf format.
-                        sockMessage.set_received_key_id_plus_1(key_id);
-                        // serialize the protobuf format to string.
-                        if( !sockMessage.SerializeToString(&recv_str))
-                        {
-                            cerr << "Failed to write protobuf" << endl;
-                            return;
-                        }
-
-                        strcpy(send_buf, recv_str.c_str());
-                        memset(recv_buf, 0, sizeof(recv_buf));
+                        perror("server in epoll, pthread_creat error!");
+                        exit(-1);
                     }
-                    else if(m_epoll_events[i].events & EPOLLOUT)
-                    {
-                        int send_count = send_data(m_epoll_events[i].data.fd, send_buf, strlen(send_buf));
-                        memset(send_buf, 0, sizeof(send_buf));
-                    }
-
+                    pthread_mutex_unlock(&thread_mutex);
                 }
             }
         }
